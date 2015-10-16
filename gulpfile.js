8ae5ab8880d2c9636f915406a25640213161319e
William Silversmith <wms@mit.edu>
2015-10-16 19:42:48 -0400

    Removed spritesheet

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var argv = require('yargs').argv,
 	babel = require("gulp-babel"),
 	shell = require('gulp-shell'),
 	GulpSSH = require('gulp-ssh'),
-	sprite = require('gulp-node-spritesheet'),
+	//sprite = require('gulp-node-spritesheet'),
 	source = require('vinyl-source-stream'),
 	buffer = require('vinyl-buffer'),
 	gutil = require('gulp-util'),
@@ -28,44 +28,44 @@ gulp.task('default', ['make']);
 
 gulp.task('make', ['images', 'animations', 'js', 'css', 'jsx' ]);
 
-gulp.task('sprite', function () {
-	gulp.src("assets/images/sprite")
-	 .pipe(sprite({
-        outputCss: 'assets/css/sprite.css',
-        selector: '.sprite',
+// gulp.task('sprite', function () {
+// 	gulp.src("assets/images/sprite")
+// 	 .pipe(sprite({
+//         outputCss: 'assets/css/sprite.css',
+//         selector: '.sprite',
 
-        // Optional ImageMagick sampling filter.
-        downsampling: "LanczosSharp",
+//         // Optional ImageMagick sampling filter.
+//         downsampling: "LanczosSharp",
 
-        // Output configurations: in this instance to output two sprite sheets,
-        // one for "legacy" (i.e. 72dpi, pixel ratio 1), and "retina" (x2).
-        // These keys (legacy, retina) are completely arbitrary.
-        output: {
-            legacy: {
-                pixelRatio: 1,
-                outputImage: 'assets/images/sprite.png',
-                // Optional path to output image
-                httpImagePath: '/images/sprite.png'
-            },
-            retina: {
-                pixelRatio: 2,
-                outputImage: 'assets/images/sprite@2x.png',
-                httpImagePath: '/images/sprite@2x.png'
-            }
-        },
+//         // Output configurations: in this instance to output two sprite sheets,
+//         // one for "legacy" (i.e. 72dpi, pixel ratio 1), and "retina" (x2).
+//         // These keys (legacy, retina) are completely arbitrary.
+//         output: {
+//             legacy: {
+//                 pixelRatio: 1,
+//                 outputImage: 'assets/images/sprite.png',
+//                 // Optional path to output image
+//                 httpImagePath: '/images/sprite.png'
+//             },
+//             retina: {
+//                 pixelRatio: 2,
+//                 outputImage: 'assets/images/sprite@2x.png',
+//                 httpImagePath: '/images/sprite@2x.png'
+//             }
+//         },
         
-        // Allows you to augment your selector names for each image, based on
-        // the bare image "name", or the full image path.
-        resolveImageSelector: function(name, fullpath) {
-            // For example, your files may well already be named with @2x, but
-            // you won't want that included in your CSS selectors.
-            return name.split('@2x').join('');
-        }
-    }))
-	.pipe(gulp.dest('assets/images/'));
-});
+//         // Allows you to augment your selector names for each image, based on
+//         // the bare image "name", or the full image path.
+//         resolveImageSelector: function(name, fullpath) {
+//             // For example, your files may well already be named with @2x, but
+//             // you won't want that included in your CSS selectors.
+//             return name.split('@2x').join('');
+//         }
+//     }))
+// 	.pipe(gulp.dest('assets/images/'));
+// });
 
-gulp.task('images', [ 'sprite' ], function () {
+gulp.task('images', [ ], function () {
 	gulp
 		.src('assets/images/**')
 		.pipe(gulp.dest('dist/public/images/'));
