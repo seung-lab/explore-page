184e7e6fef8e435664bbd5e96d3d6d5cff8a0166
Chris Jordan <jordanchriss@gmail.com>
2015-12-02 19:42:27 -0500

    reduced requirements for a swipe event and added a touch start event

--- clientjs/jquery-hammer.js
+++ clientjs/jquery-hammer.js
@@ -7,7 +7,11 @@ Hammer = propagating(Hammer);
 $(document).ready(function () {
 	let mc = new Hammer.Manager($('#viewport').get(0));
 	mc.add(new Hammer.Pan({ threshold: 0, pointers: 0 }));
-	mc.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_VERTICAL })).recognizeWith(mc.get('pan')); // todo, what does this do?
+	mc.add(new Hammer.Swipe({
+		direction: Hammer.DIRECTION_ALL,
+		velocity: 0.05, // default .65
+		threshold: 3, // default 10
+	})).recognizeWith(mc.get('pan')); // todo, what does this do?
 
 
 	mc.on('swipe', function (evt) {
@@ -16,6 +20,12 @@ $(document).ready(function () {
 
 	mc.on('panstart panmove', function (evt) {
 		$(window).trigger('panmove', evt);
+	});
+
+	mc.on("hammer.input", function(evt) {
+		if (evt.isFirst) {
+			$(window).trigger('touch', evt);
+		}
 	});
 
 	mc.on("hammer.input", function(evt) {
